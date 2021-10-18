a4ab735e215341dd02bded127f065430f1d2d47a
ITConcept2019 <92385026+ITConcept2019@users.noreply.github.com>
2021-10-18 11:33:12 +0200

    Delete force_email_cerification.js

--- force_email_cerification.js
+++
@@ -1,9 +0,0 @@
-function emailVerified(user, context, callback) {
-  if (!user.email_verified) {
-    return callback(
-      new UnauthorizedError('Please verify your email before logging in.')
-    );
-  } else {
-    return callback(null, user, context);
-  }
-}
